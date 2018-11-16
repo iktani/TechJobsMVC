fe44cbd8fa575236ac90fc1493c128de515c2902
Irwin Tani <itani05@gmail.com>
2018-11-15 20:20:40 -0600

    Base requirements completed. Program is working. No bonus missions attempted so far.

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,23 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> matches = new List<Dictionary<string, string>>();
+            if (searchType.Equals("all"))
+            {
+                matches = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                matches = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            ViewBag.jobs = matches;
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.searchType = searchType;
+            ViewBag.searchTerm = searchTerm;
+            return View("Index");
+        }
+
     }
 }
